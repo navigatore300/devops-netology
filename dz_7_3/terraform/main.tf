45a805cb1037de834747732f00fcdd2b80466da0
Maksimov Denis <navigatore300@gmail.com>
2022-08-23 12:55:27 +0300

    dz_7_3 Task 2 + count

--- dz_7_3/terraform/main.tf
+++ dz_7_3/terraform/main.tf
@@ -16,14 +16,18 @@ provider "yandex" {
 
 locals {
   instances_type_map_image = {
-    prod = "${var.ubuntu2004lts}"
+    prod = "${var.ubuntu2004lts}",
     stage = "${var.ubuntu1604lts}"
+  }
+  instances_map_count = {
+    prod = 2,
+    stage  = 1
   }
 }
 
 
-resource "yandex_compute_instance" "vm-1" {
-  name = "terraform1"
+resource "yandex_compute_instance" "vm-count" {
+  name = "terraform-${terraform.workspace}-${count.index}"
 
   resources {
     cores  = 2
@@ -38,9 +42,11 @@ resource "yandex_compute_instance" "vm-1" {
   }
 
   network_interface {
-    subnet_id = yandex_vpc_subnet.subnet-1.id
-    nat       = true
+    subnet_id = "${yandex_vpc_subnet.subnet-1.id}"
   }
+
+  // Для п.4 Добавим count.
+  count = local.instances_map_count[terraform.workspace]
 
   metadata = {
     #ssh-keys = "ubuntu:${file("~/keys_ssh.pub")}"
@@ -83,18 +89,18 @@ resource "yandex_vpc_subnet" "subnet-1" {
   v4_cidr_blocks = ["192.168.10.0/24"]
 }
 
-output "internal_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
-}
+#output "internal_ip_address_vm_1" {
+#  value = yandex_compute_instance.vm-1[count.index]
+#}
 
 #output "internal_ip_address_vm_2" {
 #  value = yandex_compute_instance.vm-2.network_interface.0.ip_address
 #}
 
 
-output "external_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
-}
+#output "external_ip_address_vm_1" {
+#  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+#}
 
 #output "external_ip_address_vm_2" {
 #  value = yandex_compute_instance.vm-2.network_interface.0.nat_ip_address
